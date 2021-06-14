269f1deb5d20d5bbd0e7d80c24efdc2679434143
epdsn <ibestudios@gmail.com>
2021-06-13 20:27:30 -0700

    Code cleanup.

--- ListAd/script.js
+++ ListAd/script.js
@@ -1,38 +1,33 @@
-(function($) {
-    function insertCards(parent, childCssClass){
-        var randomCount = getRandomInt(0,10);
-
+(function ($) {
+    function insertCards(parent, childCssClass) {
+        let randomCount = getRandomInt(0, 10);
         for (let index = 0; index <= randomCount; index++) {
             parent.append(
-                $('<div/>', {'class': childCssClass}).append(
-                    $('<span/>', {text: "Card 01-" + randomCount})
+                $('<div/>', { 'class': childCssClass }).append(
+                    $('<span/>', { text: "Card 01-" + (index + 1) })
                 )
             );
         }
     }
-
+    function PlaceAdvertisment(advertisment, target, childCssClass) {
+        let _cardLength = target.children('div.' + childCssClass).length;
+        if (_cardLength > 2) {
+            advertisment.insertAfter(target.find("div." + childCssClass + ":nth-child(3)"))
+        } else {
+            if (_cardLength > 0)
+                advertisment.appendTo(target)
+        }
+        advertisment.show();
+    }
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-
-    function PlaceAdvertisment(advertisment, target) {
-        var _cardLength = target.children('div.card').length;  
-        if (_cardLength > 2) {
-            advertisment.insertAfter(target.find("div.card:nth-child(3)"))
-        } else {
-            if (_cardLength > 0)
-                advertisment.appendTo(target)
-        }
-        advertisment.show();        
-    }
-
-    $(document).ready(function(){
+    $(document).ready(function () {
         var prependToDiv = $('#community').find('div.wrapped:visible:first');
-        var ad = $('#Ad');
+        var advertisment = $('#Ad');
         insertCards(prependToDiv, 'card')
-        PlaceAdvertisment(ad , prependToDiv);
+        PlaceAdvertisment(advertisment, prependToDiv, 'card');
     });
-
- })(jQuery);
+})(jQuery);
